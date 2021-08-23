ae8d3f6cc6f7f9b483ed4a8f0b01779d4468e37a
Adam Foerster <adam.foerster2protonmail.com>
2021-08-23 18:19:51 -0300

    feat: sync button animation

--- src/app/sync-button/sync-button.component.ts
+++ src/app/sync-button/sync-button.component.ts
@@ -1,9 +1,29 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'gms-sync-button',
   templateUrl: './sync-button.component.html',
   styleUrls: ['./sync-button.component.scss'],
+  animations: [
+    trigger('rotate', [
+      state('synced', style({ transform: 'rotate(180deg)' })),
+      state('unsynced', style({ transform: 'rotate(0deg)' })),
+      transition('synced <=> unsynced', animate('200ms ease-in-out')),
+    ]),
+    trigger('fadeOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(300, style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class SyncButtonComponent implements OnInit {
   synced = false;
